9736797942df46a152fd18e21d4029f6966b4b91
KKANGS <KKANGS@KKANGS-PC>
2014-07-31 02:28:01 +0900

    delete some file

--- CameraPreview/jni/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE    := myproc
-LOCAL_SRC_FILES := native_proc.c
-LOCAL_LDLIBS +=  -llog -ljnigraphics -lz -lm
-
-include $(BUILD_SHARED_LIBRARY)
