7a6233e0462334ebd881d12717ab366e83cde30f
KKANGS <kkangskorea@gmail.com>
2014-07-31 01:57:27 +0900

    AlarmBroadcast에서 SnapshotReceiver에 대한 sendBroadcast를 발생시켜서 SnapshotService에서 Broadcast 받도록 구현 -- 테스트 필요

--- src/com/example/timetraveler/SnapshotService.java
+++ src/com/example/timetraveler/SnapshotService.java
@@ -2,6 +2,7 @@ package com.example.timetraveler;
 
 import android.app.Service;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
@@ -10,6 +11,8 @@ import android.util.Log;
 public class SnapshotService extends Service {
 
 	public static final String LOGTAG = "TTService";
+	private IntentFilter Snapshotfilter;
+	private SnapshotReceiver ssReceiver;
 	
 	@Override
 	public IBinder onBind(Intent intent) {
@@ -19,9 +22,16 @@ public class SnapshotService extends Service {
 	
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
-		
-		Log.i(LOGTAG, "start checksizelv()");
+		/* LVSizeObserver 가동 */
 		CheckSizeLV();
+		
+		/* AlarmBroadcast에서 날라오는 Intent Broadcast에 대한 Broadcast Receiver 등록
+		 * 필요시 추가 가능 --> SnapshotReceiver class에
+		 */
+		Snapshotfilter = new IntentFilter(SnapshotReceiver.SNAPSHOT_SERVICE_SS_GENERATE_START);
+		ssReceiver = new SnapshotReceiver();
+		registerReceiver(ssReceiver, Snapshotfilter);
+		
 		
 		return startId;
 	}
@@ -35,12 +45,13 @@ public class SnapshotService extends Service {
 
 	@Override
 	public void onDestroy() {
-		
+		unregisterReceiver(ssReceiver);
 		
 	}
 	
 	/* LVSize를 체크해서 autoExtend 해주는 인스턴스 사용, 메소드 호출 */
 	private void CheckSizeLV() {
+		Log.i(LOGTAG, "start checksizelv()");
 		Handler observHandler = null;
 		
 		LVSizeObserver lvSizeObserver = new LVSizeObserver(observHandler);
